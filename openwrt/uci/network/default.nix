a0ff4b68892e68af1184781adccc7fc2094ca3b1
mateusz <mateusz.mazelanik@gmail.com>
2025-11-06 00:32:54 +0100

    add network.device options

--- openwrt/uci/network/default.nix
+++ openwrt/uci/network/default.nix
@@ -1,6 +1,6 @@
 {
   imports = [
     ./interface.nix
-    # ./device.nix
+    ./device.nix
   ];
 }
---
+++ openwrt/uci/network/device.nix
@@ -0,0 +1,128 @@
+{ lib, ... }:
+with lib;
+let
+  device = mkOption {
+    type = types.attrsOf (
+      types.submodule {
+        options = {
+          name = mkOption {
+            type = types.str;
+            example = "eth0";
+            description = ''
+              L3 device name.
+
+              Needs to match the `device` option of the respective interface section.
+            '';
+          };
+
+          macaddr = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            example = "62:11:22:aa:bb:cc";
+            description = ''
+              MAC address overriding the default one for this device.
+            '';
+          };
+
+          type = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            example = "bridge";
+            description = ''
+              Device type.  
+              If set to `bridge`, creates a bridge of the given name using L2 devices listed in `ports`
+              and wireless interfaces assigned using the `network` option in the wireless configuration.
+            '';
+          };
+
+          ifname = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            example = "eth0";
+            description = ''
+              The base L2 device required when using the `macvlan` device type.  
+              Install the `kmod-macvlan` package if necessary.
+            '';
+          };
+
+          ports = mkOption {
+            type = types.nullOr (types.listOf types.str);
+            default = null;
+            example = [
+              "eth0"
+              "eth1"
+            ];
+            description = ''
+              List of L2 device names that form part of this bridge.
+            '';
+          };
+
+          rxpause = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            example = "1";
+            description = ''
+              Controls the receive (RX) flow control.  
+              Setting it to `1` enables RX pause frames, allowing the interface to signal senders to pause
+              when overwhelmed by incoming data.
+            '';
+          };
+
+          txpause = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            example = "1";
+            description = ''
+              Controls the transmission (TX) flow control.  
+              Setting it to `1` enables TX pause frames, allowing the interface to temporarily stop sending
+              data when the receiver is overloaded.
+            '';
+          };
+
+          autoneg = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            example = "1";
+            description = ''
+              Enables auto-negotiation (`1`) to automatically determine link parameters
+              such as speed and duplex mode with the connected device.
+            '';
+          };
+
+          speed = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            example = "1000";
+            description = ''
+              Configures the link speed (e.g., 10, 100, 1000 for Mbps).
+            '';
+          };
+
+          duplex = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            example = "1";
+            description = ''
+              Configures duplex mode:  
+              `1` = Full Duplex, `0` = Half Duplex.
+            '';
+          };
+
+          table = mkOption {
+            type = types.nullOr types.str;
+            default = "10";
+            example = "100";
+            description = ''
+              When type is set to `vrf`, sets the routing table name or number.
+            '';
+          };
+        };
+      }
+    );
+    description = "Network device definitions for UCI network configuration.";
+  };
+
+in
+{
+  options.uci.settings.network.device = device;
+}
